e7d3bb624916beb9682db06bbf2e099a8e2dc2e9
charaes <romeo-weber@bluewin.ch>
2017-05-23 14:21:28 +0200

    updated

--- app/gui-model/guimodel.ts
+++ app/gui-model/guimodel.ts
@@ -2,7 +2,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Romeos Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -21,6 +21,20 @@ export class GuiModel {
                             "name": "FirstName",
                             "width": 1,
                             "required": true
+                        },
+                        { 	
+                        	"id": "nickname", 
+                        	"type": "text", 
+                        	"name": "Nickname", 
+                        	"width": 2, 
+                        	"required": true 
+                        },
+                        { 	"id": "group", 
+                        	"type": "autocomplete", 
+                        	"name": "Group", 
+                        	"data": [ "Study", "Family", "School" ], 
+                        	"form": "GroupForm", 
+                        	"width": 2 
                         },
                         {
                             "id":   "location",
@@ -82,7 +96,60 @@ export class GuiModel {
                             "name": "Ok"
                         }
                     ]
-                }
+                },
+            
+				{ 	"id": "GroupForm", 
+					"title": "Group", 
+					"formFieldList": [ 
+						{ 
+							"id": "name", 
+							"type": "text", 
+							"name": "GroupName", 
+							"width": 2, 
+							"required": true 
+						}, 
+						{ 
+							"type": "deleteButton", 
+							"name": "Delete" 
+						}, 
+						{ 
+							"type": "cancelButton", 
+							"name": "Cancel" 
+						}, 
+						{ 
+							"type": "okButton", 
+							"name": "Ok" 
+						} 
+							
+						] 
+					},
+					{ 	"id": "ActivityForm", 
+					"title": "Activities", 
+					"formFieldList": [ 
+						{ 
+							"id": "name", 
+							"type": "text", 
+							"name": "Activity", 
+							"width": 2, 
+							"required": true 
+						}, 
+						{ 
+							"type": "deleteButton", 
+							"name": "Delete" 
+						}, 
+						{ 
+							"type": "cancelButton", 
+							"name": "Cancel" 
+						}, 
+						{ 
+							"type": "okButton", 
+							"name": "Ok" 
+						} 
+							
+						] 
+					}
+
+
             ],
             "pageList": [
                 {
@@ -102,6 +169,20 @@ export class GuiModel {
                             "icon": "fa-cubes",
                             "color": "yellow",
                             "page": "locationspage",
+                        },
+                        { 
+                        	"type": "button", 
+                        	"name": "Groups", 
+                        	"icon": "fa-weixin", 
+                        	"color": "wisteria", 
+                        	"page": "groupspage", 
+                        },
+                        { 
+                        	"type": "button", 
+                        	"name": "Activities", 
+                        	"icon": "fa-heartbeat", 
+                        	"color": "yellow", 
+                        	"page": "activitypage", 
                         },
                     ]
                 },
@@ -126,9 +207,8 @@ export class GuiModel {
                             "color": "blue",
                             "search": true,
                             "data": [ { name: "Anton Amacker" }, { name: "Britta Beavers"} ],
-                            "form": {
-                                "form": "FriendForm"
-                            }
+                            "page": "UserPage",
+                            
                         },
                     ]
                 },
@@ -153,12 +233,154 @@ export class GuiModel {
                             "color": "blue",
                             "search": true,
                             "data": [ { name: "Adelboden" }, { name: "Winterthur" }, { name: "Zinal"}, { name: "Zürich"} ],
-                            "form": {
-                                "form": "LocationForm"
-                            }
+                            "page": "LocationActivityPage",
+                            
                         },
                     ]
-                }
+                },
+            
+				{ 	"id": "groupspage", 
+					"elementList": [ 
+						{
+							 "type": "backbutton", 
+						}, 
+						{ 
+							"type": "newButton", 
+							"name": "NewGroup", 
+							"icon": "fa-weixin", 
+							"color": "green", 
+							"form": { 
+								"form": "GroupForm" 
+							} 
+						}, 
+
+						{
+							"type": "list",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"search": true,
+							"data": [ { name: "Study" }, { name: "Family" }, { name: "School"} ],
+							"form": {
+								"form": "GroupForm"
+									}
+						},
+					] 
+				},
+				{ 	"id": "activitypage", 
+					"elementList": [ 
+						{
+							 "type": "backbutton", 
+						}, 
+						{ 
+							"type": "newButton", 
+							"name": "AddActivity", 
+							"icon": "fa-heartbeat", 
+							"color": "green", 
+							"form": { 
+								"form": "ActivityForm" 
+							} 
+						}, 
+
+						{
+							"type": "list",
+							"icon": "fa-heartbeat",
+							"color": "orange",
+							"search": true,
+							"data": [ { name: "Running" }, { name: "Mountainbiking" }, { name: "Skiing"} ],
+							"page": "UserActivityPage",
+									
+						},
+					] 
+				},
+				{ 	"id": "UserPage", 
+					"elementList": [ 
+						{
+							 "type": "backbutton", 
+						}, 
+						{ 
+							"type": "newButton", 
+							"name": "AddActivity", 
+							"icon": "fa-heartbeat", 
+							"color": "green", 
+							"form": { 
+								"form": "ActivityForm" 
+							} 
+						}, 
+						{ 
+							"type": "button", 
+							"name": "EditFriend", 
+							"icon": "fa-user", 
+							"color": "green", 
+							"form": { 
+								"form": "FriendForm" 
+							} 
+						},
+
+						{
+							"type": "list",
+							"icon": "fa-heartbeat",
+							"color": "orange",
+							"search": true,
+							"data": [ { name: "Running" }, { name: "Mountainbiking" }, { name: "Skiing"} ],
+							"form": {
+								"form": "ActivityForm"
+									}
+						},
+					] 
+				},
+				{
+                    "id": "UserActivityPage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewFriend",
+                            "icon": "fa-user",
+                            "color": "green",
+                            "form": {
+                                "form": "FriendForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-user",
+                            "color": "blue",
+                            "search": true,
+                            "data": [ { name: "Anton Amacker" }, { name: "Britta Beavers"} ],
+                            "page": "UserPage",
+                            
+                        },
+                    ]
+                },	
+                { 	"id": "LocationActivityPage", 
+					"elementList": [ 
+						{
+							 "type": "backbutton", 
+						}, 
+						{ 
+							"type": "newButton", 
+							"name": "AddActivity", 
+							"icon": "fa-heartbeat", 
+							"color": "green", 
+							"form": { 
+								"form": "ActivityForm" 
+							} 
+						}, 
+
+						{
+							"type": "list",
+							"icon": "fa-heartbeat",
+							"color": "orange",
+							"search": true,
+							"data": [ { name: "Mountainbiking" }, { name: "Skiing"} ],
+							"page": "UserActivityPage",
+									
+						},
+					] 
+				},
+
             ]
         }
     };
